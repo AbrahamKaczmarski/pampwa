357bf0cc6bdd7a7a5a4e07920364464b06642f00
Abraham Kaczmarski <abrahamkaczmarski@gmail.com>
2021-11-02 18:33:21 +0100

    Change paths to relative

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const cacheName = 'pam'
-const files = ['/', '/index.html', '/style.css', '/main.js']
+const files = ['./', './index.html', './style.css', './main.js']
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(files)))
